c169f1123085df7af8fcfaaf7fc1d65c51f513ac
Kyle Rose <krose@krose.org>
2019-07-31 10:30:36 -0400

    update copyright notice to be conformant

--- alta/producer.py
+++ alta/producer.py
@@ -2,7 +2,7 @@
 #
 # MIT License
 #
-# Copyright (c) Akamai Technologies, Inc. 2019
+# Copyright (C) 2019 Akamai Technologies, Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
